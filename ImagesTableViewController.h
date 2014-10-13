87ba75046a6edada1c5883daea6be323b5d97001
Sameer Totey <sameertotey@Sameers-MacBook-Pro.local>
2014-10-13 14:19:33 -0600

    Table View controller implemented

--- ImagesTableViewController.h
+++ ImagesTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ImagesTableViewController.h
 //  Blocstagram
 //
 //  Created by Sameer Totey on 10/13/14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ImagesTableViewController : UITableViewController
 
 @end
-
