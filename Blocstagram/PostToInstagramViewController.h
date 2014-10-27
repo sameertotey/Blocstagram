4592ae9fd19a36ca19b54d04d024a01d3f251dcd
Sameer Totey <sameertotey@Sameers-MacBook-Pro.local>
2014-10-27 12:37:44 -0600

    Photo Filters and Sending to Instagram

---
+++ Blocstagram/PostToInstagramViewController.h
@@ -0,0 +1,15 @@
+//
+//  PostToInstagramViewController.h
+//  Blocstagram
+//
+//  Created by Sameer Totey on 10/26/14.
+//  Copyright (c) 2014 Sameer Totey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostToInstagramViewController : UIViewController
+
+- (instancetype) initWithImage:(UIImage *)sourceImage;
+
+@end
